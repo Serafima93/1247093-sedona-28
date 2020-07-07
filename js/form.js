5f76b2b8849d4f0ae990fc098a612970e282677b
Serafima93 <65505035+Serafima93@users.noreply.github.com>
2020-07-07 13:21:42 +0300

    доппп

--- js/form.js
+++ js/form.js
@@ -1,12 +1,8 @@
-
 var button = document.querySelector(".button-search");
 var form = document.querySelector(".formsearch");
 var enter = form.querySelector(".enter");
 var sent = form.querySelector(".button");
 var exit = form.querySelector(".exit");
-
-
-
 
 button.addEventListener("click", function () {
   form.classList.add("hidden");
