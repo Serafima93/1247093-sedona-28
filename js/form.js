e0bf7c979cfe22116d2b059eb2dc661b2009b497
Serafima <65505035+Serafima93@users.noreply.github.com>
2020-07-08 12:30:15 +0300

    but

--- js/form.js
+++ js/form.js
@@ -4,12 +4,13 @@ var enter = form.querySelector(".enter");
 var sent = form.querySelector(".button");
 var exit = form.querySelector(".exit");
 
+if (button) {
 button.addEventListener("click", function () {
   form.classList.toggle("hidden");
   form.classList.toggle("visible");
   enter.focus();
 });
-
+}
 form.addEventListener("submit", function (evt) {
   if (!enter.value || !exit.value) {
     evt.preventDefault();
