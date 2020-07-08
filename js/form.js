cc23ce5838c6bd59dd46097e303180227818cac4
Serafima <65505035+Serafima93@users.noreply.github.com>
2020-07-08 15:44:41 +0300

    красиво

--- js/form.js
+++ js/form.js
@@ -1,20 +1,21 @@
-var button = document.querySelector(".button-search");
+var buttonsearch = document.querySelector(".button-search");
 var form = document.querySelector(".formsearch");
 var enter = form.querySelector(".enter");
 var sent = form.querySelector(".button");
 var exit = form.querySelector(".exit");
 
-if (button) {
-  button.addEventListener("click", function () {
-  form.classList.toggle("hidden");
-  form.classList.toggle("visible");
-  enter.focus();
-});
+if (buttonsearch) {
+    buttonsearch.addEventListener("click", function() {
+        form.classList.toggle("hidden");
+        form.classList.toggle("visible");
+        enter.focus();
+    });
 }
 if (form) {
-form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if (!(enter.value || exit.value)) {return;
-     }
-});
+    form.addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        if (!(enter.value || exit.value)) {
+            return;
+        }
+    });
 }
