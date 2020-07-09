24083f0b6e2324d212c2ac59dad3fcaa2ed572ee
Serafima93 <65505035+Serafima93@users.noreply.github.com>
2020-07-09 11:28:40 +0300

    g

--- js/form.js
+++ js/form.js
@@ -1,21 +1,21 @@
-var buttonsearch = document.querySelector(".button-search");
+var buttonSearch = document.querySelector(".button-search");
 var form = document.querySelector(".formsearch");
 var enter = form.querySelector(".enter");
 var sent = form.querySelector(".button");
 var exit = form.querySelector(".exit");
 
-if (buttonsearch) {
-    buttonsearch.addEventListener("click", function() {
+if (buttonSearch) {
+    buttonSearch.addEventListener("click", function() {
         form.classList.toggle("hidden");
         form.classList.toggle("visible");
         enter.focus();
     });
 }
 if (form) {
-    form.addEventListener("submit", function(evt) {
-        evt.preventDefault();
-        if (!(enter.value || exit.value)) {
-            return;
-        }
-    });
+  form.addEventListener("submit", function(evt) {
+      evt.preventDefault();
+      if (!(enter.value || exit.value)) {
+      return;
+      }
+  });
 }
